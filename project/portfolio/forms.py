2f060378334f2d4999f8653f65331fe09a5328ac
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-09-19 17:36:39 -0400

    Add contact form to portfolio app

---
+++ project/portfolio/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.forms import fields, widgets
+from .models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ['first_name', 'last_name', 'email', 'user_request']
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class':'input'}),
+            'last_name': forms.TextInput(attrs={'class': 'input'}),
+            'email': forms.EmailInput(attrs={'class': 'input'}),
+            'user_request': forms.Textarea(attrs={'class': 'textarea', 'rows': 10})
+        }
