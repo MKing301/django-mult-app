3957e89d576dcb2b4473fc11b8df818ee09ff644
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-09-24 17:18:44 -0400

    Add smtp config to project settings

--- project/project/settings.py
+++ project/project/settings.py
@@ -142,3 +142,11 @@ MESSAGE_TAGS = {
         messages.WARNING: 'alert-warning',
         messages.ERROR: 'alert-danger'
 }
+
+# SMTP Configuration
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('MAIL_USERNAME')
+EMAIL_HOST_PASSWORD = os.environ.get('MAIL_PASSWORD')
