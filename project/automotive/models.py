395429b7c040d8965ed4c10830946153bd9db236
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-10-05 19:53:30 -0400

    Add vehicle model to automotive app

--- project/automotive/models.py
+++ project/automotive/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Vehicle(models.Model):
+    """
+    Model defines a vehicle which has make, model, year
+    vin, license_plate and color.
+    """
+    make = models.CharField(max_length=25)
+    model = models.CharField(max_length=25)
+    year = models.CharField(max_length=4)
+    vin = models.CharField(max_length=17)
+    license_plate = models.CharField(max_length=8)
+    color = models.CharField(max_length=15)
+
+    def __str__(self):
+        """
+            Represent vehicle class object as a string consisting of the
+            year, make and model.
+            """
+        return self.year + ' ' + self.make + ' ' + self.model
