ce275e4ea19cbc8a4175792083a5e2f2a3374793
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-10-05 21:03:37 -0400

    Update comments and flake8 errors

--- project/portfolio/admin.py
+++ project/portfolio/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import Contact
-from django.db import models
 
 
 admin.site.register(Contact)
--- project/portfolio/models.py
+++ project/portfolio/models.py
@@ -3,15 +3,17 @@ from django.db import models
 
 class Contact(models.Model):
     """
-    Model defines a contact which has first name, last name, email address and request.
+    Model defines a contact which has first name, last name, email address
+    and request.
     """
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
-    user_request =  models.TextField()
+    user_request = models.TextField()
 
     def __str__(self):
         """
-        Represent Contact class object as a string consisting of the first name, space and last name.
+        Represent Contact class object as a string consisting of the
+        first name, space and last name.
         """
         return self.first_name + ' ' + self.last_name
