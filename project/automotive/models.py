398b6581eb7eb0b89f815b5fb2587ad87d3bd72f
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-10-05 20:00:20 -0400

    Add dealership model to the automotive app

--- project/automotive/models.py
+++ project/automotive/models.py
@@ -19,3 +19,23 @@ class Vehicle(models.Model):
             year, make and model.
             """
         return self.year + ' ' + self.make + ' ' + self.model
+
+
+class Dealership(models.Model):
+    """
+    Model defines a dealership which has name, address, city
+    state, postal code and office number.
+    """
+    name = models.CharField(max_length=150)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=4)
+    _state = models.CharField(max_length=17)
+    postal_code = models.CharField(max_length=8)
+    office_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        """
+            Represent dealership class object as a string consisting of the
+            dealership name.
+            """
+        return self.name
