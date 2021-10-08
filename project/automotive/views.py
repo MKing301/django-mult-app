08002dc0c6fce8de6af32eee7762b292f5606b4d
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-10-07 20:48:45 -0400

    Add log view to automotive app

--- project/automotive/views.py
+++ project/automotive/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from .models import Service
 
 
 def index(request):
     return render(
         request=request,
         template_name="automotive/index.html"
+    )
+
+
+def log(request):
+    services = Service.objects.order_by('-service_date')
+    return render(
+        request=request,
+        template_name="automotive/log.html",
+        context={'services': services}
     )
