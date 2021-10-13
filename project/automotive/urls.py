1ca769d056ba6e06b147c269500309021fa533f6
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-10-13 18:37:03 -0400

    Add path for add svc record to auto urlpatterns

--- project/automotive/urls.py
+++ project/automotive/urls.py
@@ -7,4 +7,5 @@ app_name = "automotive"
 urlpatterns = [
     path('', views.index, name="index"),
     path('log/', views.log, name="log"),
+    path('add_service/', views.add_service, name="add_service"),
 ]
