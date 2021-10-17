30a5a49cc8dd20734f27b5eb1d4746798f55c89d
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-10-17 14:42:54 -0400

    Add the add service view to the automotive app

--- project/automotive/views.py
+++ project/automotive/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .models import Service
+from django.shortcuts import render, redirect
+from .models import Service, Vehicle, Dealership, Advisor
+from .forms import ServiceForm
+from django.contrib import messages
 from django.core.paginator import Paginator
 
 
@@ -7,6 +9,38 @@ def index(request):
     return render(
         request=request,
         template_name="automotive/index.html"
+    )
+
+
+def add_service(request):
+
+    vehicles = Vehicle.objects.order_by('year', 'make')
+    dealerships = Dealership.objects.order_by('name')
+    advisors = Advisor.objects.order_by('last_name')
+
+    if request.method == 'POST':
+        form = ServiceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Service record saved successfully!')
+            return redirect('automotive:log')
+
+        else:
+            messages.error(request, 'An error occurred!')
+            return redirect('automotive:add_service')
+
+    else:
+        form = ServiceForm()
+
+    return render(
+        request=request,
+        template_name='automotive/add_service.html',
+        context={
+            'form': form,
+            'vehicles': vehicles,
+            'dealerships': dealerships,
+            'advisors': advisors
+        }
     )
 
 
