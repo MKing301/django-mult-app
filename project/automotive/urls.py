9f45347a3dfd3cac28d418dcab3d2756ac3e9305
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-10-24 13:21:27 -0400

    Add edit service path to auto app urlpatterns

--- project/automotive/urls.py
+++ project/automotive/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('log/', views.log, name="log"),
     path('add_service/', views.add_service, name="add_service"),
+    path(
+        'log/edit_service/<int:id>/',
+        views.edit_service,
+        name="edit_service"
+    ),
 ]
