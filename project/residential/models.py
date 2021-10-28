4df4f2aca0be9370fdaf433af57581ec05ea5f2e
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-10-28 17:48:56 -0400

    Add task model to residential app

--- project/residential/models.py
+++ project/residential/models.py
@@ -12,6 +12,7 @@ class Vendor(models.Model):
     vendor_state = models.CharField(max_length=2, blank=True)
     postal_code = models.CharField(max_length=8, blank=True)
     office_number = models.CharField(max_length=15, blank=True)
+    email = models.EmailField(max_length=255, blank=True)
     website = models.TextField(blank=True)
 
     def __str__(self):
@@ -20,3 +21,30 @@ class Vendor(models.Model):
         vendor's name.
         """
         return self.name
+
+
+class Task(models.Model):
+    """
+    Model defines a task performed by a vendor which has
+    task_date, name, vendor, vendor_rep, contact_num, documentation,
+    cost and notes.
+    """
+    task_date = models.DateField()
+    name = models.CharField(max_length=60)
+    vendor = models.ForeignKey(
+        Vendor,
+        blank=True,
+        on_delete=models.CASCADE
+    )
+    vendor_rep = models.CharField(max_length=50, blank=True)
+    contact_num = models.CharField(max_length=10, blank=True)
+    documentation = models.TextField(blank=True)
+    cost = models.DecimalField(blank=True, max_digits=8, decimal_places=2)
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        """
+        Represent task class object as a string consisting of the
+        task's name.
+        """
+        return str(self.name)
