60cb478d929581feeebc2c4e569766b16762bfe1
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-10-28 19:02:17 -0400

    Add edit task path to residential app urlpatterns

--- project/residential/urls.py
+++ project/residential/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('task_log/', views.task_log, name="task_log"),
     path('add_task/', views.add_task, name="add_task"),
+    path(
+        'task_log/edit_task/<int:id>/',
+        views.edit_task,
+        name="edit_task"
+    ),
 ]
