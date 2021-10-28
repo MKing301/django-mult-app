ffffa62b5abf7ede1d97bc6ec4a8bc1d84853aef
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-10-28 18:34:04 -0400

    Add the add task view to the residential app

--- project/residential/views.py
+++ project/residential/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .models import Task
+from django.shortcuts import render, redirect
+from .models import Task, Vendor
+from .forms import TaskForm
+from django.contrib import messages
 from django.core.paginator import Paginator
 
 
@@ -7,6 +9,34 @@ def index(request):
     return render(
         request=request,
         template_name="residential/index.html"
+    )
+
+
+def add_task(request):
+
+    vendors = Vendor.objects.order_by('name')
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Task saved successfully!')
+            return redirect('residential:task_log')
+
+        else:
+            messages.error(request, 'An error occurred!')
+            return redirect('residential:add_task')
+
+    else:
+        form = TaskForm()
+
+    return render(
+        request=request,
+        template_name='residential/add_task.html',
+        context={
+            'form': form,
+            'vendors': vendors
+        }
     )
 
 
