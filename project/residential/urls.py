cc684897d7f1610ee682ff5101b2c4694fb02d7f
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-10-28 18:33:11 -0400

    Add the add task path to resident app urlpatterns

--- project/residential/urls.py
+++ project/residential/urls.py
@@ -7,4 +7,5 @@ app_name = "residential"
 urlpatterns = [
     path('', views.index, name="index"),
     path('task_log/', views.task_log, name="task_log"),
+    path('add_task/', views.add_task, name="add_task"),
 ]
