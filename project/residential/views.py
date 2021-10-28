8cd582b045d6b33e9b111c08186755f749251f46
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-10-28 17:54:20 -0400

    Add task log view to residential app

--- project/residential/views.py
+++ project/residential/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from .models import Task
+from django.core.paginator import Paginator
 
 
 def index(request):
     return render(
         request=request,
         template_name="residential/index.html"
+    )
+
+
+def task_log(request):
+    # Set up pagination
+    p = Paginator(Task.objects.order_by('-task_date'), 5)
+    page = request.GET.get('page')
+    tasks = p.get_page(page)
+
+    return render(
+        request=request,
+        template_name="residential/task_log.html",
+        context={'tasks': tasks}
     )
