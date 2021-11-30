33294372d38f2818aa900223bd576429048b0647
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-11-30 17:43:01 -0500

    Add test case to residential app for task model

--- project/residential/tests.py
+++ project/residential/tests.py
@@ -1,5 +1,7 @@
+import datetime
+
 from django.test import TestCase
-from .models import Vendor
+from .models import Vendor, Task
 
 
 class VendorTestCase(TestCase):
@@ -19,3 +21,30 @@ class VendorTestCase(TestCase):
         company = self.vendor
         self.assertTrue(isinstance(company, Vendor))
         self.assertEqual(str(company), "BioTurf")
+
+
+class TaskTestCase(TestCase):
+    def test_task_str(self):
+        _vendor = Vendor.objects.create(
+            name="SpringGreen",
+            address="1234 Main St",
+            city="Raleigh",
+            vendor_state="NC",
+            postal_code="27601",
+            office_number="9197779311",
+            email="sp@company.com",
+            website="sp.com"
+        )
+
+        _task = Task.objects.create(
+            task_date=datetime.date(1997, 10, 19),
+            name="Test a task",
+            vendor=_vendor,
+            vendor_rep="Jane Doe",
+            contact_num="9197779311",
+            documentation="",
+            cost=0.00,
+            notes="This is a test."
+        )
+
+        self.assertEqual(str(_task), "Test a task")
