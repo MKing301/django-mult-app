337de32749c0fd9272fdf8c313a8d473aca4031c
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-11-29 21:21:36 -0500

    Add test case for portfolio app contact model

--- project/portfolio/tests.py
+++ project/portfolio/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Contact
 
-# Create your tests here.
+
+class ContactTestCase(TestCase):
+    def setUp(self):
+        self.contact = Contact.objects.create(
+            first_name="Jane",
+            last_name="Doe",
+            email="jane.doe@gmail.com",
+            user_request="This is a test"
+        )
+
+    def test_contact_str(self):
+        user = self.contact
+        self.assertTrue(isinstance(user, Contact))
+        self.assertEqual(str(user), "Jane Doe")
