7d2c4569e7cf2e9fc35fe6bffd4c87c12193e709
MKING301 <33675411+MKing301@users.noreply.github.com>
2021-11-30 17:15:16 -0500

    Add test case to residential app for vendor model

--- project/residential/tests.py
+++ project/residential/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Vendor
 
-# Create your tests here.
+
+class VendorTestCase(TestCase):
+    def setUp(self):
+        self.vendor = Vendor.objects.create(
+            name="BioTurf",
+            address="1234 Main St",
+            city="Raleigh",
+            vendor_state="NC",
+            postal_code="27601",
+            office_number="9197779311",
+            email="bioturf@company.com",
+            website="bioturf.com"
+        )
+
+    def test_vendor_str(self):
+        company = self.vendor
+        self.assertTrue(isinstance(company, Vendor))
+        self.assertEqual(str(company), "BioTurf")
