0354655bc37375226806f37b6e1fbee321ec690c
MKING301 <33675411+MKing301@users.noreply.github.com>
2023-12-26 16:25:42 +0000

    Update settings

--- project/project/settings.py
+++ project/project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY_MULT')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.77', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['192.168.1.155']
 
 
 # Application definition
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
