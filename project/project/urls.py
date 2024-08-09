eac3d47829b4c85e59daf44399e82103895d1355
MKING301 <33675411+MKing301@users.noreply.github.com>
2024-08-09 16:56:43 -0400

    Update project urls with path for expense tracking app

--- project/project/urls.py
+++ project/project/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('autos/',  include('automotive.urls')),
     path('residential/',  include('residential.urls')),
+    path('expense_tracking/',  include('expense_tracking.urls')),
 ]
