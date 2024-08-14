9613bf3cdf700e6916bc9837efd774fc0684da54
MKING301 <33675411+MKing301@users.noreply.github.com>
2024-08-14 16:48:49 -0400

    Update url paths for residential app

--- project/residential/urls.py
+++ project/residential/urls.py
@@ -6,6 +6,8 @@ app_name = "residential"
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path("accounts/logout/", views.logout_request, name="logout_request"),
+    path("accounts/login/", views.login_request, name="login_request"),
     path('task_log/', views.task_log, name="task_log"),
     path('add_task/', views.add_task, name="add_task"),
     path(
